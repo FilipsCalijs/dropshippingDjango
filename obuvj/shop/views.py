4f4e2ecab21862173795bb70139ba46d8b36a801
FilipsCalijs <filips.calijs@gmail.com>
2023-02-02 21:05:40 +0200

    Полностью закончил курс по Джанго
    Все, я его закончил, я делал его с 12.01.2023 -02.02.2023 (21 недель). Сайт сделан очень классно, в нем уже почти все кроме оплаты и еще некоторых вещей работает! Ссылка на курс:   https://youtu.be/4enaYwCZ1Go

--- obuvj/shop/views.py
+++ obuvj/shop/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView, DeleteView
 
+from shop.forms import AddQuantityForm
+from shop.models import Product, Order, OrderItem
 from shop.scraping import scraping, ScrapingError
 
 
@@ -12,3 +18,61 @@ def fill_database(request):
             return render(request, 'shop/fill-products.html', {'message': str(err)})
 
     return render(request, 'shop/fill-products.html', {'message': None})
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'shop/shop.html'
+
+class ProductsDetailView(DetailView):
+        model = Product
+        template_name = 'shop/shop-details.html'
+
+
+@login_required(login_url=reverse_lazy('login'))
+def add_item_to_cart(request, pk):
+    if request.method == 'POST':
+        quantity_form = AddQuantityForm(request.POST)
+        if quantity_form.is_valid():
+            quantity = quantity_form.cleaned_data['quantity']
+            if quantity:
+                cart = Order.get_cart(request.user)
+                # product = Product.objects.get(pk=pk)
+                product = get_object_or_404(Product, pk=pk)
+                cart.orderitem_set.create(product=product,
+                                          quantity=quantity,
+                                          price=product.price)
+                cart.save()
+                return redirect('cart_view')
+        else:
+            pass
+    return redirect('shop')
+
+@login_required(login_url=reverse_lazy('login'))
+def cart_view(request):
+    cart = Order.get_cart(request.user)
+    items = cart.orderitem_set.all()
+    context = {
+        'cart': cart,
+        'items': items,
+    }
+    return render(request, 'shop/cart.html', context)
+
+@method_decorator(login_required, name='dispatch')
+class CartDeleteItem(DeleteView):
+    model = OrderItem
+    template_name = 'shop/cart.html'
+    success_url = reverse_lazy('cart_view')
+
+    # Проверка доступа
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs.filter(order__user=self.request.user)
+        return qs
+
+
+@login_required(login_url=reverse_lazy('login'))
+def make_order(request):
+    cart = Order.get_cart(request.user)
+    cart.make_order()
+    return redirect('shop')
